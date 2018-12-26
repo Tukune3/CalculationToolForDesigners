32ea05ed7885a0c95b5512e8ee5854acc09cfda8
Tukune3 <taira@genius-web.co.jp>
2018-12-26 14:22:03 +0900

    サイト名の変更と不要ファイルの削除

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="robots" content="noindex">
+    <title>ジーニアスウェブ・ツールズ</title>
+    
+    <link rel="stylesheet" href="./assets/css/app.css">
+    <link rel="stylesheet" href="./assets/css/fox.css">
+    <link rel="stylesheet" href="./assets/css/fox.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script type="text/javascript" src="./assets/js/app.js"></script>
+</head>
+<body id="listStyle">
+    
+    <div id="listStage">
+        <div class="section">
+            <h1>ジーニアスウェブ・ツールズ</h1>
+            <p>
+                ※便利なツール一覧です
+            </p>
+            
+            <ul>
+                <li><a href="./tagchecker/">CS Checker</a></li>
+                <li><a href="./calculation_tool_for_designers/">Calculation tool for designers</a></li>
+            </ul>
+            
+        </div>
+    </div>
+    
+</body>
+</html>
